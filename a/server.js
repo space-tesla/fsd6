a7eccd0371b5d7573668412459e77fb430413aab
space-tesla <aspyrontech@gmail.com>
2025-04-21 21:58:06 +0530

    files added successfully

---
+++ a/server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+http.createServer((req, res) => {
+  let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('404 Not Found');
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    }
+  });
+}).listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
